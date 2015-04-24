0aec1a399cbae876a2c6e0d7000b2437e952a80e
Bryan Knouse <bryan.knouse@gmail.com>
2015-04-24 07:42:05 -0400

    my persona page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessible :name, :email, :password, :password_confirmation
+	attr_accessible :name, :email, :reputation, :password, :password_confirmation
 	has_secure_password
 
 	before_save { self.email = email.downcase }
