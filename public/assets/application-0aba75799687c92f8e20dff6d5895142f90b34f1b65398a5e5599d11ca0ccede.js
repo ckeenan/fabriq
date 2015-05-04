cbd224d74627da3d321db7832d3f54bdf20c7cc8
Bryan Knouse <bryan.knouse@gmail.com>
2015-05-04 15:53:33 -0400

    badges, tribes

--- public/assets/application-0aba75799687c92f8e20dff6d5895142f90b34f1b65398a5e5599d11ca0ccede.js
+++ public/assets/application-0aba75799687c92f8e20dff6d5895142f90b34f1b65398a5e5599d11ca0ccede.js
@@ -10834,6 +10834,10 @@ return jQuery;
 
 
 }).call(this);
+(function() {
+
+
+}).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
