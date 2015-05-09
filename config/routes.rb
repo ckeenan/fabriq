f62bf51691936a20220d75b766bcb53f57a638aa
Bryan Knouse <bryan.knouse@gmail.com>
2015-05-09 17:46:46 -0400

    basic landing page with design

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'main#index'
+  root 'main#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
