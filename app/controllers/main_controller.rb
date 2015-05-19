ee9ebdeb61e6707d49896aee3083837934682205
Bryan Knouse <bryan.knouse@gmail.com>
2015-05-19 14:09:59 -0400

    landing and explore page UI

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,11 +8,13 @@ class MainController < ApplicationController
   end
 
   def home
+    
     @newsletter = Newsletter.new
   	render :layout => "landing"
   end
 
   def explore
+    @top_reps = User.all.order('reputation DESC').limit(5)
     @users = User.all
     @array = @users.map{|f| [f.name, f.avatar.to_s, f.id]}
     @arrayb = ["Bryan", "Ashley", "Patrick", "Connor", "Carolyn"]
